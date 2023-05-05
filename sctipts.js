5e1d74856dae7f5c60e1efa37441dd0f98e0b8fd
Viktor Lukyanchenkov <viktor.lukyanchenkov@cd.p2h.com>
2023-05-05 11:23:38 +0300

    03 work with dom

--- sctipts.js
+++
@@ -1,6 +0,0 @@
-const values = [2, 14, 20, 50]
-
-const newValues = values.map((value, index) => {
-    let newValue = value + 2
-    console.log('new value: ' + newValue + ' ,index number: ' + (index + 1))
-})
